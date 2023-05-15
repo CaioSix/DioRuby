617d2e2cb579797ee72598cae52edcbf4bdff235
Caio Cavalcanti <C0650904@vale.com>
2023-05-15 13:28:50 -0300

    update course

---
+++ aula4.rb
@@ -0,0 +1,11 @@
+print "Digite seu nome: ";
+nome = gets.chomp
+
+print "Digite seu sobre nome: "
+sobrenome = gets.chomp
+
+print "Qual sua idade? "
+idade = gets.chomp.to_i
+
+
+print "Oie #{nome} #{sobrenome} bem vindo ao meu hello world, voce tem #{idade} anos"
